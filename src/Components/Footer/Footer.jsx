971275e238f7bd67e2499a5a84f3db46a075265c
Subir Das <dasjoy104@gamil.com>
2024-07-04 00:16:38 +0600

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../../assets/Seventh-section-img/Group 1.png';
-import { FaFacebook,FaInstagram  } from "react-icons/fa6";
+import { FaFacebook,FaInstagram, FaLinkedin , FaYoutube  } from "react-icons/fa6";
 
 const Footer = () => {
   return (
    <div className='bg-[#020043] p-20 text-white'>
      <img src={logo} alt="" />
-    <div className='grid grid-cols-4 '>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 '>
       <div>
         
         <p><p className='my-6'>123 Main Street Anytown, USA <br/> Postal Code: 12345</p>Support: support@oyolloo.com <br />
@@ -26,12 +26,14 @@ const Footer = () => {
       </div>
       <div className='my-6 '>
         
-        <p className='my-4'>Follow Us</p>
-        <div>
-        <FaFacebook />
-        <FaInstagram />
+        <p className='mb-4'>Follow Us</p>
+        <div className='flex gap-6 '>
+        <FaFacebook className='text-2xl'/>
+        <FaInstagram className='text-2xl'/>
+        <FaLinkedin className='text-2xl'/>
+        <FaYoutube className='text-2xl'/>
         </div>
-        <p className='mt-4'>@Docplus 2024</p>
+        <p className='mt-12'>@Docplus 2024</p>
       </div>
     </div>
    </div>
